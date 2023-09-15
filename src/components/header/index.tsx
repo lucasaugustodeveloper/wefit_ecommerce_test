b5d6903d1cd0e20f72e919f1002fab5dfd2576fc
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-09-15 12:35:42 -0300

    add component header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styled'
+
+export default function Header() {
+  return (
+    <S.Header>
+      <S.Title>WeMovies</S.Title>
+    </S.Header>
+  )
+}
---
+++ src/components/header/styled.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin: 1.125rem 0.625rem;
+
+  background-color: red;
+`
+
+export const Title = styled.h1`
+  font-size: 1.25rem;
+
+  color: #ffffff;
+`
