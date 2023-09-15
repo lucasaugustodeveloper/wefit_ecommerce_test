c96ea489fff5632718b6bcf77f72ab474e2277bb
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-09-15 15:05:37 -0300

    add component of loading

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styled'
+
+export default function Loading() {
+  return (
+    <S.Loading>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </S.Loading>
+  )
+}
