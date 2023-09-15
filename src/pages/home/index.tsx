6131c50cf3ef86363a6ae7b9a09c045680761590
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-09-15 15:06:35 -0300

    add component of header and card

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,38 @@
+import { useState, useEffect } from 'react'
+
+import Card, { CardProps } from '../../components/card'
 import Header from '../../components/header'
+import Loading from '../../components/loading'
+
+import * as S from './styled'
 
 function App() {
+  const [cards, setCards] = useState<CardProps[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/products')
+    .then(res => res.json())
+    .then(data => setCards(data))
+  }, [])
+
   return (
     <div>
       <Header />
+
+      <S.Container cards={cards.length}>
+        {!cards.length ? (
+          <Loading />
+        ) : (
+          cards.map(card => 
+            <Card
+              key={card.id}
+              image={card.image}
+              title={card.title}
+              price={card.price}
+            />
+          )
+        )}
+      </S.Container>
     </div>
   )
 }
